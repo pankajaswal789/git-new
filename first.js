564de476163a2d5463c9ed9897e5a5357eb67d3c
Pankaj Aswal <pankajaswal789@gmail.com>
2025-04-04 22:43:42 +0530

    final

--- first.js
+++ first.js
@@ -1,18 +1,3 @@
 const name = "pankaj";
-const age=25
-<<<<<<< HEAD
-let balance=4000;
-
-function Accbalance(value){
-
-balance +=value;
-
-}
-
-
-Accbalance(10);
-
-console.log("account balance is",balance);
-=======
-let balance=4000;
->>>>>>> parent of eb65aa4 (second commint)
+const age=24;
+let balace=4000
